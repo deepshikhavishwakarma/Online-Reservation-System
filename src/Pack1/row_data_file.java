c330cd0304c081838d96f1e347450e6c70cce4d3
deepshikhavishwakarma <deepshikha2000deep@gmail.com>
2023-07-31 14:11:33 +0530

    First Commit

---
+++ src/Pack1/row_data_file.java
@@ -0,0 +1,26 @@
+
+//-----------------create table --------
+//create table traindetails(
+//tno int(11) PRIMARY KEY,
+//tname varchar(255) not null,
+//startplace varchar(255) not null,
+//destinationplace varchar(255) not null,
+//price int(11)
+//
+//);
+
+
+
+//create table reservation(
+//    pno int(11) PRIMARY KEY not null,
+//    splace varchar(255) not null,
+//    eplace varchar(255) not null,
+//    tno int(11) not null,
+//    tname varchar(255) not null,
+//    price int(11) not null,
+//    date varchar(255) not null,
+//    ticket int(11) not null,
+//    tot int(11) not null
+//
+//
+//);
